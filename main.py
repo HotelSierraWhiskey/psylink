24e67e19683f143640d1dbaa0bd055121382f223
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-09-03 17:20:58 -0400

    importing config directly

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from worker import worker_task
-from config import get_config, logger
+from config import config, logger
 
-
-config = get_config()
 
 if __name__ == '__main__':
-    logger.info("Starting Psylink worker...")
-    logger.info(f"Running on {config.connection.hostname} ({config.connection.ip})")
+    logger.info(
+        f"Starting Psylink (Worker UID: {config.properties.worker_id})")
+    logger.info(
+        f"Running on {config.connection.hostname} ({config.connection.ip})")
+
     while True:
         try:
             worker_task()
