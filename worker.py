fe93e093c5e109b9504cdbffa73daf15e3935163
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-09-02 20:37:00 -0400

    implemented priorities

--- worker.py
+++ worker.py
@@ -1,6 +1,8 @@
 from config import get_config
 from db import get_db
 from llm import get_llm
+import json
+import socket
 
 
 config = get_config()
@@ -9,6 +11,17 @@ llm = get_llm()
 
 
 def worker_task():
-    _, prompt = db.brpop(config.redis.input_queue)
-    output = llm(str(prompt))
-    db.lpush(config.redis.output_queue, str(output))
+    _, input_data = db.brpop(config.redis.input_queue)
+    json_data = json.loads(input_data.decode('utf-8').replace("'", '"'))
+    priority = json_data['priority']
+    prompt = json_data['input']
+
+    
+    if not config.properties.max_priority >= priority >= config.properties.min_priority:
+        return
+
+    hostname = socket.gethostname()
+    
+    output = llm(prompt, **config.llama_params)
+    payload = str({'hostname': hostname, 'input': prompt, 'output': output})
+    db.lpush(config.redis.output_queue, payload)
