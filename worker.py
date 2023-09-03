c3ff5076b4deb69f977776a3ce227a4a6109690f
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-09-03 12:55:17 -0400

    added id field to payload

--- worker.py
+++ worker.py
@@ -1,9 +1,7 @@
-from config import get_config
+from config import get_config, logger
 from db import get_db
 from llm import get_llm
 import json
-import socket
-from log import logger
 
 
 config = get_config()
@@ -14,19 +12,22 @@ llm = get_llm()
 def worker_task():
     _, input_data = db.brpop(config.redis.input_queue)
     json_data = json.loads(input_data.decode('utf-8').replace("'", '"'))
-    logger.info("Received data from input queue")
+
+    id = json_data['id']
     priority = json_data['priority']
     prompt = json_data['input']
 
-    
     if not config.properties.max_priority >= priority >= config.properties.min_priority:
-        db.lpush(config.redis.input_queue)
-        logger.info(f"Priority mismatch")
+        db.lpush(config.redis.input_queue, json.dumps(json_data))
         return
 
-    hostname = socket.gethostname()
-    
+    logger.info(f"Pulled message {id} from input queue")
+
     output = llm(prompt, **config.llama_params)
-    logger.info(f"processed prompt: {output}")
-    payload = str({'hostname': hostname, 'input': prompt, 'output': output})
+
+    logger.info(f"Done. {output}")
+
+    payload = json.dumps({'id': id, 'hostname': config.connection.hostname,
+                          'input': prompt, 'output': output})
+
     db.lpush(config.redis.output_queue, payload)
