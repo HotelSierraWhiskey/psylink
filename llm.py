9078fcc725ced78f738224210befc32a2013d3a1
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-09-02 20:36:46 -0400

    rename

--- llm.py
+++ llm.py
@@ -4,5 +4,5 @@ from llama_cpp import Llama
 
 def get_llm():
     config = get_config()
-    llm = Llama(model_path=config.llama.model_path, verbose=False)
+    llm = Llama(model_path=config.model.model_path, verbose=False)
     return llm
