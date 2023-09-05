6ad0b18ba069436ce2d1a1ff1f044a4cf969dc31
HotelSierraWhiskey <winstontothemoon@gmail.com>
2023-09-05 08:00:29 -0400

    cleanup

--- db.py
+++ db.py
@@ -1,9 +1,8 @@
-from config import get_config
+from config import config
 import redis
 
 
 def get_db():
-    config = get_config()
     db = redis.StrictRedis(host=config.redis.host,
                            port=config.redis.port,
                            db=config.redis.db,
